9fc78a885a53039242672e287ba8eb91d60fae9e
Cody Lance <50344757+OneChanceLance@users.noreply.github.com>
2023-06-13 15:24:33 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import { Typography } from "@material-ui/core/Typography";
 import { useState } from "react";
-import { Selection } from "./ListOfPeople";
+import { Selection } from "./ListOfItems";
 
 function App() {
   const [alignment, setAlignment] = useState("");
@@ -22,15 +22,41 @@ function App() {
   );
 }
 
+
+
 export default function IsInStock() {
   const itemsInStock = Selection.filter((i) => i.amountInStock >= 1);
-  const listItems = itemsInStock.map((ix) => (
-    <li>
+  const itemsOutOfStock = Selection.filter((i) => i.amountInStock == 0);
+  const listItemsInStock = itemsInStock.map((ix) => (
       <p>
-        <h2>{ix.itemName}</h2>
-        <b>Status: In Stock</b>
+       <h2 align={"center"}>{ix.itemName}</h2>
+        <b align={"center"}>Status: In Stock</b>
       </p>
-    </li>
+ ));
+  const listItemsNotInStock = itemsOutOfStock.map((ix) => (
+     <p>
+       <h2 align={"center"}>{ix.itemName}</h2>
+        <b align={"center"} type="color" value="#FF0000">Status: Out Of Stock</b>
+      </p>
   ));
-  return <ul>{listItems}</ul>;
+ // const [nameOfItem, stockStatus] = {(nameOfItem => Selection.itemName)} {stockStatus => {
+  //  if (Selection.amountInStock >= 1) {
+    //  stockStatus = "In Stock"
+  //    }
+  //  else {
+   //   stockStatus = "Out Of Stock"
+  //  }
+ // }
+ //   <li>
+  //    <p>
+  //     <h2>{i.itemName}</h2>
+ //    </p>
+  //  </li>
+ // );
+  return (
+    <div>
+      <ul>{listItemsInStock}</ul>
+      <ul>{listItemsNotInStock}</ul>
+    </div>
+    );
 }
